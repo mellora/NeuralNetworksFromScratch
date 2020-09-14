b00db0e04d2729f44e66bd961c44b323023ca28c
Alexander Mellor <AlexMellor2014@gmail.com>
2020-09-14 14:25:03 -0500

    Finished the code for the part

--- code/P5_Hidden_Layer_Activation_Functions.py
+++ code/P5_Hidden_Layer_Activation_Functions.py
@@ -1,32 +1,38 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-x = [[1, 2, 3, 2.5],
-     [2, 5, -1, 2],
-     [-1.5, 2.7, 3.3, -0.8]]
+nnfs.init()
+
+X, y = spiral_data(100, 3)
 
 
-inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
-output = []
+class LayerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+        self.output = None  # Added line to prevent warning.
 
-for i in inputs:
-    output.append(max(0, i))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
 
-print(output)
+    def backward(self):
+        pass
 
-# class LayerDense:
-#     def __init__(self, n_inputs, n_neurons):
-#         self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
-#         self.biases = np.zeros((1, n_neurons))
-#         self.output = None  # Added line to prevent warning.
-#
-#     def forward(self, inputs):
-#         self.output = np.dot(inputs, self.weights) + self.biases
-#
-#
-# if __name__ == "__main__":  # Added to code
-#     layer_1 = LayerDense(4, 5)
-#     layer_2 = LayerDense(5, 2)
-#
-#     layer_1.forward(x)
-#     layer_2.forward(layer_1.output)
-#     print(layer_2.output)
+
+class ActivationReLU:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+if __name__ == "__main__":
+    layer_1 = LayerDense(2, 5)
+    activation_1 = ActivationReLU()
+
+    layer_1.forward(X)
+    activation_1.forward(layer_1.output)
+
+    print(activation_1.output)
