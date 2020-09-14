b56648c29fc1276927335de6ff287a18366d567b
Alexander Mellor <AlexMellor2014@gmail.com>
2020-09-14 12:39:24 -0500

    Modified code to work with numpy
    Did not fully convert code to follow the video.  Modeling one neuron by using the index vs removing the variables in the lists  from the code.

--- code/P3_The_dot_Product.py
+++ code/P3_The_dot_Product.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 inputs = [1, 2, 3, 2.5]
 
 weights = [[0.2, 0.8, -0.5, 1],
@@ -6,12 +8,5 @@ weights = [[0.2, 0.8, -0.5, 1],
 
 biases = [2, 3, 0.5]
 
-layer_outputs = []  # Output of current layer
-for neuron_weights, neuron_bias in zip(weights, biases):
-    neuron_output = 0   # Output of given neuron
-    for n_input, weight in zip(inputs, neuron_weights):
-        neuron_output += n_input * weight
-    neuron_output += neuron_bias
-    layer_outputs.append(neuron_output)
-
-print(layer_outputs)
+output = np.dot(weights[0], inputs) + biases[0]
+print(output)
